740c5f127ccc02bd893934f4a2c257bbb650f79c
wangdong <wangdong>
2023-08-25 00:30:59 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,10 @@ fn parse_hex32_string_to_u8_vec(hex_str: &str) -> Vec<u8> {
     // 返回结果vec
     result
 }
-fn main() {
-    let mut a = "00 0c 00 07 6e 61 73 2d 35 67 73 00 00 00 00 7e 00 68 01 00 65 2e 01 01 c2 11 00 09 01 00 06 31 3f 01 01 ff 01 06 06 13 88 04 7a 12 59 32 29 05 01 ac 1a 64 65 22 01 01 79 00 06 01 20 41 01 01 09 7b 00 18 80 80 21 0a 03 00 00 0a 81 06 08 08 08 08 00 0d 04 08 08 08 08 00 11 00 25 1c 08 69 6e 74 65 72 6e 65 74 06 6d 6e 63 30 30 31 06 6d 63 63 30 30 31 04 67 70 72 73 12 01";
-    let hex = parse_hex32_string_to_u8_vec(a);
+fn nas_5gs_decoder(nas_hex: Vec<u8>) -> Result<serde_json::Value, serde_json::Error> {
+    let prefix: Vec<u8>  = vec![0x00,0x0c,0x00,0x07,0x6e,0x61,0x73,0x2d,0x35,0x67,0x73,0x00,0x00,0x00,0x00];
+    let mut result = prefix.clone();
+    result.extend(nas_hex);
     let header = pcap_file::pcap::PcapHeader {
         version_major: 2,
         version_minor: 4,
@@ -40,10 +41,10 @@ fn main() {
     let now = SystemTime::now()
     .duration_since(UNIX_EPOCH)
     .expect("Time went backwards");
-    let mut pP = pcap::PcapPacket { timestamp: now, orig_len: hex.len() as u32, data: std::borrow::Cow::Borrowed(&hex)};
+    let mut pP = pcap::PcapPacket { timestamp: now, orig_len: result.len() as u32, data: std::borrow::Cow::Borrowed(&result)};
     writer
                         .write_packet(
-                            &pcap::PcapPacket { timestamp: now, orig_len: hex.len() as u32, data: std::borrow::Cow::Borrowed(&hex) }
+                            &pcap::PcapPacket { timestamp: now, orig_len: result.len() as u32, data: std::borrow::Cow::Borrowed(&result) }
                         )
                         .unwrap();
     let mut tshark_process = std::process::Command::new("tshark")
@@ -54,7 +55,13 @@ fn main() {
     let output = io::BufReader::new(stdout).lines();
     let output_str = output.collect::<Result<Vec<_>, _>>().unwrap()
     .join("\n");
-    let json: serde_json::Value = serde_json::from_str(&output_str).unwrap();
-    println!("{}",json);
+    let res: serde_json::Value =  serde_json::from_str(&output_str).unwrap_or_default();
+    Ok(res)
+}
+fn main() {
+    let mut a = "7e 00 68 01 00 65 2e 01 01 c2 11 00 09 01 00 06 31 3f 01 01 ff 01 06 06 13 88 04 7a 12 59 32 29 05 01 ac 1a 64 65 22 01 01 79 00 06 01 20 41 01 01 09 7b 00 18 80 80 21 0a 03 00 00 0a 81 06 08 08 08 08 00 0d 04 08 08 08 08 00 11 00 25 1c 09 69 69 6e 74 65 72 6e 65 74 06 6d 6e 63 30 30 31 06 6d 63 63 30 30 31 04 67 70 72 73 12 01";
+    let hex = parse_hex32_string_to_u8_vec(a);
+    
+    println!("{}",nas_5gs_decoder(hex).unwrap());
 
 }
